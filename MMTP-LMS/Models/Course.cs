76b9c775a63493936b437a6de5e21837028d24df
Per Orest <per.orest@outlook.com>
2019-02-28 17:09:01 +0100

    Fixed some isues with AdminFrontPage

--- MMTP-LMS/Models/Course.cs
+++ MMTP-LMS/Models/Course.cs
@@ -14,6 +14,7 @@ namespace MMTP_LMS.Models
         [Display(Name = "Description")]
         public string Description { get; set; }
         [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
         [Display(Name = "End Date")]
         [DataType(DataType.Date)]
