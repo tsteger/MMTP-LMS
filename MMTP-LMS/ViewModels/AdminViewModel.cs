dea2054610f2d1d354479c1fe5a99bee98128f12
Per Orest <per.orest@outlook.com>
2019-02-28 11:17:46 +0100

    AdminController

--- MMTP-LMS/ViewModels/AdminViewModel.cs
+++ MMTP-LMS/ViewModels/AdminViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MMTP_LMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace MMTP_LMS.ViewModels
 {
     public class AdminViewModel
     {
+        public ICollection<Person> People { get; set; }
+        public ICollection<Course> Courses { get; set; }
+        public ICollection<Module> Modules { get; set; }
+        public ICollection<LmsActivity> LmsActivities { get; set; }
+        public ICollection<Document> Documents { get; set; }
     }
 }
