1a4e894cb823e185cf8796f02a669277b72fa42e
Tommy Steger <tommysteger@gmail.com>
2019-03-05 16:16:03 +0100

    somstuff

--- MMTP-LMS/Startup.cs
+++ MMTP-LMS/Startup.cs
@@ -100,7 +100,7 @@ namespace MMTP_LMS
             //Assign Admin role to the main User here we have given our newly registered 
             //login id for Admin management
             
-            Person user = await UserManager.FindByEmailAsync("per.orest@srhlit.se");
+            Person user = await UserManager.FindByEmailAsync("Mmtp@Lexicon.se");
             if (user == null)
                 return;
             var User = new Person();
