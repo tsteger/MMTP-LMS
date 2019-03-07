641c31e578c0e9cbeaf432d968a9008120702b57
Per Orest <per.orest@outlook.com>
2019-03-07 12:02:39 +0100

    NuSÅ ska det funka med TEACHERKnappen MÅÅÅÅÅSEN !

---
+++ MMTP-LMS/Controllers/TeacherController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MMTP_LMS.Data;
+using MMTP_LMS.Models;
+using MMTP_LMS.ViewModels;
+
+namespace MMTP_LMS.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class TeacherController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Person> _userManager;
+
+        public TeacherController(ApplicationDbContext context, UserManager<Person> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var courses = _context.Course;
+
+            var getCourses = courses.Select(x => new TeacherViewModel()
+            {
+                CourseName = x.Name,
+                CourseDescription = x.Description,
+                CourseStartDate = x.StartDate,
+                CourseEndDate = x.EndDate,
+            });
+            return View(getCourses);
+        }
+
+        public IActionResult CreateCourse()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCourse([Bind("Id,Name,Description,StartDate,EndDate")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Course.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+        // GET: Course/EditCourse/5
+        public async Task<IActionResult> EditCourse(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Course.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return View(course);
+        }
+
+        // POST: Course/EditCourse/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditCourse(int id, [Bind("Id,Name,Description,StartDate,EndDate")] Course course)
+        {
+            if (id != course.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(course);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(course.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+        private bool CourseExists(int id)
+        {
+            return _context.Course.Any(e => e.Id == id);
+        }
+    }
+}
