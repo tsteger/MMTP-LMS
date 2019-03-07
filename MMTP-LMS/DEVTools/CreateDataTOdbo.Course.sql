3dc947501523a3ef2aa37ca488f32e333a6857eb
Per Orest <per.orest@outlook.com>
2019-03-07 11:24:58 +0100

    Comitted Måsens JOB

---
+++ MMTP-LMS/DEVTools/CreateDataTOdbo.Course.sql
@@ -0,0 +1,16 @@
+﻿USE [aspnet-MMTP_LMS-5EEECC18-AFA0-44E1-99CE-4107B79B1534]
+GO
+
+/****** Object: Table [dbo].[Course] Script Date: 2019-03-07 11:04:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET IDENTITY_INSERT [dbo].[Course] ON
+INSERT INTO [dbo].[Course] ([Id], [Name], [Description], [StartDate], [EndDate]) VALUES (1, N'Na18 .Net', N'C# Asp-Net Core ', N'2019-03-01 00:00:00', N'2020-03-04 00:00:00')
+INSERT INTO [dbo].[Course] ([Id], [Name], [Description], [StartDate], [EndDate]) VALUES (2, N'Na18 Java', N'Java Course', N'2019-03-01 00:00:00', N'2020-03-01 00:00:00')
+INSERT INTO [dbo].[Course] ([Id], [Name], [Description], [StartDate], [EndDate]) VALUES (3, N'Na-Spesial', N'Make Your Food', N'2019-03-01 00:00:00', N'2020-03-01 00:00:00')
+SET IDENTITY_INSERT [dbo].[Course] OFF
+
