4191d3d94f73d12c6436b0aef4b012966b8ad7df
Tommy Steger <tommysteger@gmail.com>
2019-03-07 10:50:41 +0100

    Db Scripts

---
+++ MMTP-LMS/DEVTools/T.dbo.Course.data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Course] ON
+INSERT INTO [dbo].[Course] ([Id], [Name], [Description], [StartDate], [EndDate]) VALUES (1, N'Na18 .Net', N'C# Asp-Net Core ', N'2019-03-01 00:00:00', N'2020-03-04 00:00:00')
+INSERT INTO [dbo].[Course] ([Id], [Name], [Description], [StartDate], [EndDate]) VALUES (2, N'Na18 Java', N'Java Course', N'2019-03-01 00:00:00', N'2020-03-01 00:00:00')
+INSERT INTO [dbo].[Course] ([Id], [Name], [Description], [StartDate], [EndDate]) VALUES (3, N'Na-Spesial', N'Make Your Food', N'2019-03-01 00:00:00', N'2020-03-01 00:00:00')
+SET IDENTITY_INSERT [dbo].[Course] OFF
