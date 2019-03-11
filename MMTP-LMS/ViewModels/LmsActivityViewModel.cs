7d5944d52bd8f3e7a0d9abac4360c8ed7dbd32e7
Per Orest <per.orest@outlook.com>
2019-03-11 16:14:15 +0100

    Commitade Create LmsActivity

--- MMTP-LMS/ViewModels/LmsActivityViewModel.cs
+++ MMTP-LMS/ViewModels/LmsActivityViewModel.cs
@@ -10,25 +10,25 @@ namespace MMTP_LMS.ViewModels
     public class LmsActivityViewModel
     {
         [Display(Name = "Activity Name")]
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string LmsActivityName { get; set; }
+        public string LmsActivityDescription { get; set; }
         [Display(Name = "Start Date")]
         [DataType(DataType.Date)]
-        public DateTime StartDate { get; set; }
+        public DateTime LmsActivityStartDate { get; set; }
         [Display(Name = "End Date")]
         [DataType(DataType.Date)]
-        public DateTime EndTime { get; set; }
+        public DateTime LmsActivityEndTime { get; set; }
         [Display(Name = "Activity Type")]
         public LmsActivityType LmsActivityType { get; set; }
         public int LmsActivityTypeId { get; set; }
-
+        public int LmsActivityId { get; set; }
         public int ModuleId { get; set; }
         [Display(Name = "Module Name")]
         public Module Module { get; set; }
-
+       
         [Display(Name = "Documents")]
         public ICollection<Document> Documents { get; set; }
-
+        public ICollection<Module> Modules { get; set; }
         public ICollection<LmsActivity> LmsActivities { get; set; }
     }
 }
