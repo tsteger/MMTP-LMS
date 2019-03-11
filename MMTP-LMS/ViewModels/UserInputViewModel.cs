05e28a733d5303e1efed7eafc4c3f5d4532a6c13
Tommy Steger <tommysteger@gmail.com>
2019-03-11 16:13:14 +0100

    Bugg fix error check password

--- MMTP-LMS/ViewModels/UserInputViewModel.cs
+++ MMTP-LMS/ViewModels/UserInputViewModel.cs
@@ -24,6 +24,7 @@ namespace MMTP_LMS.ViewModels
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [RegularExpression(@"(?=.*\d)(?=.*[\W_]).{6,}", ErrorMessage = "Characters are not allowed.")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
