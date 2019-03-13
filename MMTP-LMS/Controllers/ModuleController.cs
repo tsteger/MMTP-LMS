4602a6dfc26ecfb8ab9d3e9550d97a6d5d071225
Per Orest <per.orest@outlook.com>
2019-03-13 11:06:57 +0100

    ModuleControllersettings

--- MMTP-LMS/Controllers/ModuleController.cs
+++ MMTP-LMS/Controllers/ModuleController.cs
@@ -108,7 +108,7 @@ namespace MMTP_LMS.Controllers
         // POST: Module/EditModule/
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditModule(int id, [Bind("Id,ModuleName,ModuleDescription,ModuleStartDate,ModuleEndDate,CourseId")] Module module)
+        public async Task<IActionResult> EditModule(int id, [Bind("Id,Name,Description,StartDate,EndDate")] Module module)
         {
             if (id != module.Id)
             {
