b3360f63c7b33c0b205b0444db63924bc8c153ce
tsteger <tommysteger@gmail.com>
2019-03-16 18:13:05 +0100

    Admin Add Documents, whith ugly buttons

--- MMTP-LMS/Controllers/ModuleController.cs
+++ MMTP-LMS/Controllers/ModuleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,17 +15,20 @@ using MMTP_LMS.ViewModels;
 
 namespace MMTP_LMS.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ModuleController : Controller
     {
         
         private readonly ApplicationDbContext _context;
         private readonly UserManager<Person> _userManager;
+        private readonly IHostingEnvironment _hostingEnvironment;
         private DateUtilities dateUtilities;
         private static int retViewId;
-        public ModuleController(ApplicationDbContext context, UserManager<Person> userManager)
+        public ModuleController(ApplicationDbContext context, UserManager<Person> userManager, IHostingEnvironment hostingEnvironment)
         {
             _context = context;
             _userManager = userManager;
+            _hostingEnvironment = hostingEnvironment;
             dateUtilities = new DateUtilities();
         }
 
@@ -175,6 +179,31 @@ namespace MMTP_LMS.Controllers
             {
                 return View();
             }
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file, string txt, string module_id)
+        {
+
+            if (!int.TryParse(module_id, out int id)) return NoContent();
+
+            var fileUtil = new Utilities.File();
+            await fileUtil.UploadFileAsync(file, txt, _hostingEnvironment);
+            var doc = new Document
+            {
+                Name = file.FileName,
+                Description = "Module: " + _context.Module.Where(c => c.Id == id).Select(n => n.Name).FirstOrDefault(),
+                TimeStamp = DateTime.Now,
+                UserName = User.Identity.Name,
+                Url = "\\Documents\\" + file.FileName,
+                IsAdmin = true,
+                ModuleId = id,
+                PersonId = _context.Person.Where(p => p.UserName == User.Identity.Name).Select(i => i.Id).FirstOrDefault()
+            };
+
+            _context.Document.Add(doc);
+            _context.SaveChanges();
+
+            return RedirectToAction("CreateModule", "Module", new { Id = retViewId });
         }
     }
 }
